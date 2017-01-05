0fa824463dbd46e651f97fcc88f88dfa03eda6d5
George Datseris <george.datseris@ds.mpg.de>
2017-01-05 14:34:49 +0100

    removed comma that caused error

--- src/DynamicalBilliards.jl
+++ src/DynamicalBilliards.jl
@@ -8,6 +8,6 @@ include("PlotBilliards.jl")
 export Particle, MagneticParticle, cyclotron, Disk, Circle, Antidot, FiniteWall, Obstacle,
        NullWall, PeriodicWall, normalvec, resolvecollision!, distance, randominside,
        billiard_rectangle, billiard_sinai, billiard_sinai_periodic,
-       collisiontime, propagate!, evolve!, construct,
+       collisiontime, propagate!, evolve!, construct
 
 end # module
