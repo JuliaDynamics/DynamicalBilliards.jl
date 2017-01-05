43f57b127c70024be94b043e1069f8e752a56982
George Datseris <george.datseris@ds.mpg.de>
2017-01-05 14:19:44 +0100

    Added includes in main module

--- src/DynamicalBilliards.jl
+++ src/DynamicalBilliards.jl
@@ -1,5 +1,13 @@
 module DynamicalBilliards
 
-# package code goes here
+include("Collisions.jl")
+include("Propagation.jl")
+#include("StandardBilliards.jl")
+include("PlotBilliards.jl")
+
+export Particle, MagneticParticle, cyclotron, Disk, Circle, Antidot, FiniteWall, Obstacle,
+       NullWall, PeriodicWall, normalvec, resolvecollision!, distance, randominside,
+       billiard_rectangle, billiard_sinai, billiard_sinai_periodic,
+       collisiontime, propagate!, evolve!, construct,
 
 end # module
