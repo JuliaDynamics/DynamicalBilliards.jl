42d5eca86aa7e6b33ca340f6dd84818e42c61304
George Datseris <george.datseris@ds.mpg.de>
2017-01-22 13:32:25 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,8 @@
 using Documenter, DynamicalBilliards
-
-makedocs()
+ 
+makedocs(modules=[DynamicalBilliards], doctest=true)
+ 
+deploydocs(deps   = Deps.pip("mkdocs", "python-markdown-math"),
+    repo = "github.com/Datseris/DynamicalBilliards.git",
+    julia  = "0.5.0",
+    osname = "windows")
