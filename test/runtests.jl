a4218ff3a2ef425896d50c5a242f8523bd7125ca
George Datseris <george.datseris@ds.mpg.de>
2017-01-23 14:45:31 +0100

    Made runtests accept ENV variables
    Added options to give the possibility for a user to choose if tests print information and if the tests will have the long version or the short version done. 
    
    The module also now has a function `test_options` that sets those `ENV` variables for the user.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,27 @@
 using DynamicalBilliards
 using Base.Test
+# Test options:
+printinfo = false
+longtests = false
 
-# write your own tests here
-@test 1 == 2
+# Get tests options:
+if haskey(ENV, "DYNAMICALBILLIARDS_PRINTTEST")
+  if ENV["DYNAMICALBILLIARDS_PRINTTEST") == true
+      printinfo = true
+    end
+end
+
+if haskey(ENV, "DYNAMICALBILLIARDS_LONGTEST")
+  if ENV["DYNAMICALBILLIARDS_LONGTEST") == true
+      printinfo = true
+    end
+end
+
+if longtests
+    include("longtests.jl")
+else
+    include("shorttests.jl")
+end
+
+# Perform tests:
+@test check_straight_sinai_periodic(printinfo = printinfo)
