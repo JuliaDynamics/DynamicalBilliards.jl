7e8f4f5124e65a775432b038483aae4a6242d9d2
George Datseris <george.datseris@ds.mpg.de>
2017-01-23 15:04:43 +0100

    Added test functions
    I think the file is finalized now

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,14 +6,14 @@ longtests = false
 
 # Get tests options:
 if haskey(ENV, "DYNAMICALBILLIARDS_PRINTTEST")
-  if ENV["DYNAMICALBILLIARDS_PRINTTEST"] == true
+  if ENV["DYNAMICALBILLIARDS_PRINTTEST"] == "true"
       printinfo = true
     end
 end
 
 if haskey(ENV, "DYNAMICALBILLIARDS_LONGTEST")
-  if ENV["DYNAMICALBILLIARDS_LONGTEST"] == true
-      printinfo = true
+  if ENV["DYNAMICALBILLIARDS_LONGTEST"] == "true"
+      longtests = true
     end
 end
 
@@ -21,7 +21,15 @@ if longtests
     include("longtests.jl")
 else
     include("shorttests.jl")
-end
+endst
 
 # Perform tests:
+@test check_straight_sinai(printinfo = printinfo)
+@test check_magnetic_sinai(printinfo = printinfo)
 @test check_straight_sinai_periodic(printinfo = printinfo)
+@test check_magnetic_sinai_periodic(printinfo = printinfo)
+@test check_magnetic_pinned(printinfo = printinfo)
+@test check_previous_obstacle(printinfo = printinfo)
+@test check_raysplitting_omega(printinfo = printinfo)
+@test check_raysplitting_periodic(printinfo = printinfo)
+@test check_splitterwall(printinfo = printinfo)
