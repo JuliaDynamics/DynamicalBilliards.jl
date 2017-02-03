0670605e0482adb155ca4ff4024f033733bfb40f
George Datseris <datseris.george@gmail.com>
2017-02-03 23:58:56 +0100

    added , "pygments" in Deps.pip

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter, DynamicalBilliards
 makedocs(modules=[DynamicalBilliards], doctest=false)
 
 deploydocs(
-    deps   = Deps.pip("mkdocs", "mkdocs-material" ,"python-markdown-math"),
+    deps   = Deps.pip("mkdocs", "mkdocs-material" ,"python-markdown-math", "pygments"),
     repo   = "github.com/Datseris/DynamicalBilliards.jl.git",
     julia  = "0.5",
     osname = "linux"
