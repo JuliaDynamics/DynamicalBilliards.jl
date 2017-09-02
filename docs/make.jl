fd9035ec9f0dfaaf2d899a94bab7e51ce7610b30
Datseris <datseris.george@gmail.com>
2017-09-02 09:23:09 +0200

    test inline math

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,8 @@ using Documenter, DynamicalBilliards
 makedocs(modules=[DynamicalBilliards], doctest=false)
 
 deploydocs(
-    deps   = Deps.pip("mkdocs", "mkdocs-material" ,"python-markdown-math", "pygments"),
+    deps   = Deps.pip("mkdocs",
+    "mkdocs-material" ,"python-markdown-math", "pygments", "pymdown-extensions"),
     repo   = "github.com/Datseris/DynamicalBilliards.jl.git",
     julia  = "0.6",
     osname = "linux"
