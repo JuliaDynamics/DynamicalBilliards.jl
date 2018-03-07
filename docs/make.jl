ecfd0b0953268c7c164ac86e098f0736cb66a365
George Datseris <datseris.george@gmail.com>
2018-03-07 12:45:25 +0100

    fix tornado

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,7 @@ using Documenter
 makedocs(modules=[DynamicalBilliards], doctest=false)
 
 deploydocs(
-    deps   = Deps.pip("mkdocs",
+    deps   = Deps.pip(("Tornado>=4.0.0,<5.0.0", "mkdocs",
     "mkdocs-material" ,"python-markdown-math", "pygments", "pymdown-extensions"),
     repo   = "github.com/JuliaDynamics/DynamicalBilliards.jl.git",
     julia  = "0.6",
