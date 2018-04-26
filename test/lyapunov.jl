ae165bcbbf9514d4ec50b4c60f84d425ed21bb75
Lukas Hupe <lukas.hupe@t-online.de>
2018-04-26 13:08:57 +0200

    Added simple test

--- test/lyapunov.jl
+++ test/lyapunov.jl
@@ -51,3 +51,34 @@ if printinfo
 end
 return
 end
+
+function lyapunov_magnetic(partnum=500; printinfo = true)
+tim = time()
+partnum= min(10, partnum)
+@testset "Lyapunov Spectrum (magnetic)" begin
+    @testset "Sinai" begin
+        bt = billiard_sinai()
+        tt=10000.0
+        for i in 1:partnum
+            p = randominside(bt)
+            exps = lyapunovspectrum!(p, bt, tt)
+            error_level = 1e-2
+            sumpair = exps[1] + exps[4]
+
+            @test abs(sumpair) < error_level
+            @test abs(exps[2]) < error_level
+            @test abs(exps[3]) < error_level
+        end#particle loop
+    end
+end
+if printinfo
+    println("""Results:
+                + lyapunovspectrum() call works on
+                  magnetic Sinai billiard.
+                + λ₁ + λ₄ ≈ 0.
+                + λ₂ ≈ λ₃ ≈ 0.
+                + Required time: $(round(time()-tim, 3)) sec.
+                """) 
+end
+return
+end
