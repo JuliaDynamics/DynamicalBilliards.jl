7553332ce5d4c5945981ee482333c54e8fba519f
Lukas Hupe <lukas.hupe@t-online.de>
2018-05-31 20:14:26 +0200

    Removed unecessary `print` from tests

--- test/lyapunov.jl
+++ test/lyapunov.jl
@@ -1,55 +1,55 @@
 using Base.Test
 
 function lyapunov_spectrum(partnum=500; printinfo = true)
-tim = time()
-partnum= min(10, partnum)
-@testset "Lyapunov Spectrum (straight)" begin
+    tim = time()
+    partnum= min(10, partnum)
+    @testset "Lyapunov Spectrum (straight)" begin
 
 
-    @testset "Hexagonal Periodic" begin
-        l = 2.0
-        r = 1.0
-        sides = 6
-        bt = billiard_hexagonal_sinai(r,l; setting = "periodic")
-        #disc = Disk([0., 0.], r)
-        #push!(bt, disc)
-        tt=10000.0
+        @testset "Hexagonal Periodic" begin
+            l = 2.0
+            r = 1.0
+            sides = 6
+            bt = billiard_hexagonal_sinai(r,l; setting = "periodic")
+            #disc = Disk([0., 0.], r)
+            #push!(bt, disc)
+            tt=10000.0
 
-        for i in 1:partnum
-            p = randominside(bt)
-            exps = lyapunovspectrum!(p, bt, tt)
-            error_level = 1e-2
-            sumpair = exps[1] + exps[4]
+            for i in 1:partnum
+                p = randominside(bt)
+                exps = lyapunovspectrum!(p, bt, tt)
+                error_level = 1e-2
+                sumpair = exps[1] + exps[4]
 
-            @test abs(sumpair) < error_level
-            @test abs(exps[2]) < error_level
-            @test abs(exps[3]) < error_level
-        end#particle loop
+                @test abs(sumpair) < error_level
+                @test abs(exps[2]) < error_level
+                @test abs(exps[3]) < error_level
+            end#particle loop
+        end
+        @testset "Sinai" begin
+            bt = billiard_sinai()
+            tt=10000.0
+            for i in 1:partnum
+                p = randominside(bt)
+                exps = lyapunovspectrum!(p, bt, tt)
+                error_level = 1e-2
+                sumpair = exps[1] + exps[4]
+
+                @test abs(sumpair) < error_level
+                @test abs(exps[2]) < error_level
+                @test abs(exps[3]) < error_level
+            end#particle loop
+        end
     end
-    @testset "Sinai" begin
-        bt = billiard_sinai()
-        tt=10000.0
-        for i in 1:partnum
-            p = randominside(bt)
-            exps = lyapunovspectrum!(p, bt, tt)
-            error_level = 1e-2
-            sumpair = exps[1] + exps[4]
-
-            @test abs(sumpair) < error_level
-            @test abs(exps[2]) < error_level
-            @test abs(exps[3]) < error_level
-        end#particle loop
+    if printinfo
+        println("Results:")
+        println("+ lyapunovspectrum!() call works on")
+        println("  hexagonal lorentz and sinai billiard.")
+        println("+ λ₁ + λ₄ ≈ 0.")
+        println("+ λ₂ ≈ λ₃ ≈ 0.")
+        println("+ Required time: $(round(time()-tim, 3)) sec.")
     end
-end
-if printinfo
-    println("Results:")
-    println("+ lyapunovspectrum!() call works on")
-    println("  hexagonal lorentz and sinai billiard.")
-    println("+ λ₁ + λ₄ ≈ 0.")
-    println("+ λ₂ ≈ λ₃ ≈ 0.")
-    println("+ Required time: $(round(time()-tim, 3)) sec.")
-end
-return
+    return
 end
 
 function lyapunov_magnetic(partnum=500; printinfo = true)
@@ -112,7 +112,6 @@ function lyapunov_magnetic(partnum=500; printinfo = true)
                 emagsum += emag[1]; elinsum += elin[1]
             end #particle loop
             difference = emagsum - elinsum
-            print(difference)
             @test abs(difference) < partnum*error_level
         end
 
