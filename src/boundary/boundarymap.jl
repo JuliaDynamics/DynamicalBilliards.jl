d539a69d1a8d2cc70442496f34bef347035bc9ed
Lukas Hupe <lukas.hupe@t-online.de>
2018-06-21 14:38:00 +0200

    add note to docstring
    i have used up all my creativity for today so this line is not perfect

--- src/boundary/boundarymap.jl
+++ src/boundary/boundarymap.jl
@@ -87,8 +87,10 @@ end
 
 """
     from_bcoords(ξ, sφ, o::Obstacle) -> pos, vel
-Convert the boundary coordinates `ξ, φs` on the obstacle to
+Convert the boundary coordinates `ξ, sφ` on the obstacle to
 real coordinates `pos, vel`.
+
+Note that `vel` always points away from the obstacle.
 
 This function is the inverse of [`to_bcoords`](@ref).
 """
