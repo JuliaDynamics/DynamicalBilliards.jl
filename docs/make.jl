d05ec1cee9bbf0d31afc30e73e768c18cc298629
Datseris <datseris.george@gmail.com>
2018-06-23 11:43:21 +0200

    test doc deployment

--- docs/make.jl
+++ docs/make.jl
@@ -9,6 +9,6 @@ deploydocs(
     deps   = Deps.pip("Tornado>=4.0.0,<5.0.0", "mkdocs",
     "mkdocs-material" ,"python-markdown-math", "pygments", "pymdown-extensions"),
     repo   = "github.com/JuliaDynamics/DynamicalBilliards.jl.git",
-    julia  = "0.7",
+    julia  = "nightly",
     osname = "linux"
 )
