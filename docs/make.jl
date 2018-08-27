60410ffab9c02601d6b8f8008069444365f39959
George Datseris <datseris.george@gmail.com>
2018-08-27 13:04:27 +0300

    fix deps

--- docs/make.jl
+++ docs/make.jl
@@ -19,9 +19,9 @@ makedocs(modules=[DynamicalBilliards], doctest=false, root = @__DIR__)
 close("all")
 
 deploydocs(
-    deps   = Deps.pip("Tornado>=4.0.0,<5.0.0", "mkdocs",
-    "mkdocs-material" ,"python-markdown-math", "pygments", "pymdown-extensions"),
+    deps   = Deps.pip("mkdocs==0.17.5", "mkdocs-material==2.9.4",
+    "python-markdown-math", "pygments", "pymdown-extensions"),
     repo   = "github.com/JuliaDynamics/DynamicalBilliards.jl.git",
-    julia  = "nightly",
+    julia  = "1.0",
     osname = "linux"
 )
