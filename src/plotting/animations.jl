23ecc16676ee803f786624a77ede654a82fcf1e6
George Datseris <datseris.george@gmail.com>
2018-10-20 11:33:32 +0200

    save animation using FFMPEG
    something is wrong, I can't delete figures afterwards.

--- src/plotting/animations.jl
+++ src/plotting/animations.jl
@@ -48,14 +48,15 @@ end
 Animate the evolution of a vector of particles `ps` in billiard `bd` for a
 total time `t` (always considered float). Optionally enable ray-splitting.
 
-### Keyword Arguments
-  * `dt = 0.02` : Time resolution used for production of time series (see
+### Evolution kwargs
+  * `dt = 0.01` : Time resolution used for production of time series (see
     [`timeseries`](@ref). It is not recommended to significantly increase this value,
     to preserve the smoothness of the orbits.
   * `frameskip = 5` : The amount of `dt`-steps performed each frame.
     Increasing either `frameskip` and `dt` makes the animation progress faster.
   * `tailtime = 1.0` : The length of the "tail" trailing the particle in time
     units.
+### Colors & plotting kwargs
   * `colors` : An array of valid Matplotlib colors for the "tails". If `colors`
     is shorter than `ps`, colors are reused. Defaults to the standard
     Matplotlib color sequence.
@@ -63,19 +64,35 @@ total time `t` (always considered float). Optionally enable ray-splitting.
     `plot` function for particles.
   * `tail_kwargs::NamedTuple`: Additional keyword arguments passed to the `plot`
     function for "tails" (line plot).
+### Exporting and axis kwargs
+  * `figsize = (7.2, 7.2))` : Size for new figure (if one is created).
+    Must be divisible by 2 if you want to save the animation.
+  * `ax = (figure(figsize = figsize); plot(bd); gca())` : axis to plot on.
+  * `savename = nothing` : If given the animation is exported to
+    mp4 file (requires ffmpeg). The name can include path.
+  * `disable_axis = false` : Remove the axis splines.
+  * `deletefigs = true` : To create the animation a lot of figures are saved in
+    the save directory and are deleted after the animation is done. You can choose
+    to keep them.
+  * `dpi = 100` : dpi of saved figures.
+  * `framerate = 60` : Animation framerate.
 """
 function animate_evolution(ps::AbstractVector{<:AbstractParticle{T}},
                            bd::Billiard, t, raysplitters = nothing;
-                           dt  = 0.02, frameskip = 5, tailtime = 1.0,
+                           dt  = 0.01, frameskip = 5, tailtime = 1.0,
                            colors = ["C$i" for i ∈ 0:9],
                            particle_kwargs = NamedTuple(),
-                           tail_kwargs = NamedTuple()) where {T}
+                           tail_kwargs = NamedTuple(),
+                           figsize = (7.2, 7.2),
+                           ax = (PyPlot.figure(figsize = figsize); ax = gca(); plot(bd; ax = ax); ax),
+                           savename = nothing, dpi = 100, deletefigs = true,
+                           disable_axis = false, framerate = 60
+                           ) where {T}
 
+    disable_axis && ax[:axis]("off")
     nps = length(ps)
     taillength = round(Int, tailtime/dt)
-
-    ax = PyPlot.gca()
-    plot(bd, ax = ax)
+    savename != nothing && (colnumbers = Int[])
 
     plotframes = Vector{Function}(undef, nps)
     skipframes = Vector{Function}(undef, nps)
@@ -102,6 +119,25 @@ function animate_evolution(ps::AbstractVector{<:AbstractParticle{T}},
             sleep(0.01)
         else
             [sf() for sf in skipframes]
+        end
+        if savename != nothing
+            s = savename*"_$(i).png"
+            PyPlot.savefig(s, dpi = dpi)
+            push!(colnumbers, i)
+        end
+    end
+
+    if savename != nothing
+        @assert mod(figsize[1]*dpi, 2) == 0
+        @assert mod(figsize[2]*dpi, 2) == 0
+        anim = `ffmpeg -y -framerate $(framerate) -start_number 1 -i $(savename)_%d.png
+        -c:v libx264 -pix_fmt yuv420p -preset veryslow -profile:v high -level 5.2 $(savename).mp4`
+        run(anim)
+
+        if deletefigs
+            for i in colnumbers
+                rm(savename*"_$(i).png")
+            end
         end
     end
 end
