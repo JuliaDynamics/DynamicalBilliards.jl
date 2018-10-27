23e705147ec43bba6cf9dffddb4653f09c841532
George Datseris <datseris.george@gmail.com>
2018-10-27 18:39:11 +0200

    typos, typos everywhere

--- src/raysplitting.jl
+++ src/raysplitting.jl
@@ -400,8 +400,8 @@ end
 """
     law_of_refraction(n1, n2 = 1.0) -> t, r
 
-Create a transmission and refraction functions `t, r` that follow Snell's
-refraction law, i.e. the transmission probability is set to 1.0 except
+Create transmission and refraction functions `t, r` that follow Snell's
+law, i.e. the transmission probability is set to 1.0 except
 for the case of total internal reflection. 
 
 `n1` is the index of refraction for the `pflag = false` side of an obstacle,
