76cb313f7e844d1905372b1281ff1442c18e835e
George Datseris <datseris.george@gmail.com>
2019-05-14 14:23:57 +0200

    remove dates

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,8 @@
 using DynamicalBilliards
-using Dates
 using Test
 using DynamicalBilliards.Testing
 
-print("DynamicalBilliards tests start at: ")
-print(Dates.format(now(), "HH:MM:s"), "\n")
+print("DynamicalBilliards tests start now.")
 timetime = time()
 
 include("basic_tests.jl")
@@ -15,8 +13,7 @@ include("phasespaces_tests.jl")
 include("raysplt_tests.jl")
 include("lyapunov.jl")
 
-print("\nDynamicalBilliards tests ended (successfully) at: ")
-print(Dates.format(now(), "HH:MM:s"), "\n")
+print("\nDynamicalBilliards tests ended (successfully)!")
 timetime = time() - timetime
 println("Total time required was:")
 println(round(timetime, digits = 3), " seconds, or ", round(timetime/60, digits=3), " minutes")
