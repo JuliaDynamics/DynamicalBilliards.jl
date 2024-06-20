a4eb24f9ad8529d47b46f6ce28ceb17c08f79cc0
Datseris <datseris.george@gmail.com>
2024-06-20 21:15:32 +0100

    port visualization doc file

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,44 @@
 cd(@__DIR__)
-using Pkg
-CI = get(ENV, "CI", nothing) == "true" || get(ENV, "GITHUB_TOKEN", nothing) !== nothing
-CI && Pkg.activate(@__DIR__)
-CI && Pkg.instantiate()
 
-using DynamicalBilliards
-using Documenter, DocumenterTools, Literate
-using InteractiveDynamics, CairoMakie
+using DynamicalSystems
+# TODO: cross-reference docstirngs directly into package's repos!
 
-# %%
+import Downloads
+Downloads.download(
+    "https://raw.githubusercontent.com/JuliaDynamics/doctheme/master/build_docs_with_style.jl",
+    joinpath(@__DIR__, "build_docs_with_style.jl")
+)
+include("build_docs_with_style.jl")
+
+using Literate
+infile = joinpath(pkgdir("src", "billiards_visualizations.jl")
+outdir = joinpath(@__DIR__, "src")
+Literate.markdown(infile, outdir; credit = false)
+
+pages = [
+    "Introduction" => "index.md",
+    "High Level API" => "basic/high_level.md",
+    "Visualizing & Animating" => "billiards_visualizations.md",
+    "Phase Spaces" => "basic/phasespaces.md",
+    "Ray-Splitting" => "ray-splitting.md",
+    "Lyapunov Exponents" => "lyapunovs.md",
+    "MushroomTools" => "mushroomtools.md",
+    "Physics" => "physics.md",
+    "Internals" => "basic/low_level.md",
+    "Tutorials" => [
+        "Defining a Billiard" => "tutorials/billiard_table.md",
+        "Defining your own Obstacles" => "tutorials/own_obstacle.md",
+    ]
+]
+
+
+build_docs_with_style(pages, DynamicalBilliards;
+    authors = "George Datseris <datseris.george@gmail.com>",
+    expandfirst = ["index.md"],
+)
+
+
+
 using DocumenterTools: Themes
 # download the themes
 for file in ("juliadynamics-lightdefs.scss", "juliadynamics-darkdefs.scss", "juliadynamics-style.scss")
